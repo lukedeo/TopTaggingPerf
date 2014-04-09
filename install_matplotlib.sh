1e7f1ed54090f50d17e987555d6d64d386171d9d
lukedeo <lukepercivaldeoliveira@gmail.com>
2014-04-09 02:24:53 -0400

    minor

--- install_matplotlib.sh
+++ install_matplotlib.sh
@@ -6,14 +6,12 @@ tar -xzvf mock-1.0.1.tar.gz
 cd mock-1.0.1
 python setup.py install --user
 cd ..
-
 #Install PyParsing
 wget https://pypi.python.org/packages/source/p/pyparsing/pyparsing-2.0.1.tar.gz#md5=37adec94104b98591507218bc82e7c31 --no-check-certificate
 tar -xzvf pyparsing-2.0.1.tar.gz 
 cd pyparsing-2.0.1
 python setup.py install --user
 cd ..
-
 #install six
 wget https://pypi.python.org/packages/source/s/six/six-1.6.1.tar.gz#md5=07d606ac08595d795bf926cc9985674f --no-check-certificate
 tar -xzvf six-1.6.1.tar.gz
@@ -26,7 +24,6 @@ tar -xzvf python-dateutil-1.5.tar.gz
 cd python-dateutil-1.5
 python setup.py install --user
 cd ..
-
 
 git clone git@github.com:matplotlib/matplotlib.git
 cd matplotlib
