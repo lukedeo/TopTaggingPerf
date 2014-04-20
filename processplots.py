e25cbd3141734095576fb3a8ebdb661c9c5f23df
lukedeo <lukepercivaldeoliveira@gmail.com>
2014-04-20 17:49:47 -0400

    processing script

---
+++ processplots.py
@@ -0,0 +1,17 @@
+import sys
+import yaml
+import TopTaggingPerf as tt
+import datetime
+
+
+if __name__ == '__main__':
+	filename = str(sys.argv[0])
+	f = open(filename, 'r')
+	schema = yaml.load(f)
+	
+	taggers = tt.generate_taggers(schema)
+
+	now = datetime.datetime.now()
+	timestamp = now.strftime("%Y-%m-%d_%H:%M")
+
+	tt.plot_roc(taggers, schema, 'ROC_plots_' + timestamp + '.pdf')
